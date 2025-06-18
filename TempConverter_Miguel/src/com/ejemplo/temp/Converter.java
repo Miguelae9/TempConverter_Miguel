5e1236f5939bdb4fe037f24c811d071cbd6959aa
Miguel Angel <mpergar0904@g.educaand.es>
2025-06-18 16:55:26 +0200

    feature: añade fahrenheitToCelsius y prueba

--- TempConverter_Miguel/src/com/ejemplo/temp/Converter.java
+++ TempConverter_Miguel/src/com/ejemplo/temp/Converter.java
@@ -9,5 +9,12 @@ public class Converter {
     public static void main(String[] args) {
         Converter cv = new Converter();
         System.out.println("0 °C = " + cv.celsiusToFahrenheit(0) + " °F");
+        System.out.println("32 °F = " + cv.fahrenheitToCelsius(32) + " °C");
     }
+    
+ // Dentro de la clase Converter
+    public double fahrenheitToCelsius(double f) {
+        return (f - 32) * 5/9;
+    }
+
 }
